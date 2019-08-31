8f483838806c731544e7ee4736d808b47b316267
Danny Nani <danny880@gmail.com>
2019-08-31 23:49:25 +0300

    add solution 6

--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+
+argNum=$#
+whereTo=${!#}
+fileNum=$((${argNum}-1))
+hostname="server2"
+
+if [ "$HOSTNAME" = "server2" ]
+        then
+                hostname="server1"
+fi
+
+for i in `seq 1 $fileNum`;
+        do
+                filePath=${!i}
+                sshpass -p "vagrant" scp $filePath vagrant@$hostname:$whereTo
+        done
+
